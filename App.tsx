fa4d7516c102b922e5d78d755d662286f51d90fa
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2022-05-26 13:50:08 +0800

    feature/Enable Nativebase

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import AppContainer from "./src/components/app-container";
 export default function App() {
   return (
-    <View style={styles.container}>
+    <AppContainer>
+      <View style={styles.container}>
       <Text>Open up App.tsx to start workings on yosur app!</Text>
+      <Text>
+        Hello Worlds
+      </Text>
       <StatusBar style="auto" />
     </View>
+    </AppContainer>
   );
 }
 
