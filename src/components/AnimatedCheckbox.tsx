5ebffafaedfece0ba343f297e17a81c5f4d1d09b
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2022-05-28 21:30:55 +0800

    added bezier and linear from Easing of react-natiev-reanimated

--- src/components/AnimatedCheckbox.tsx
+++ src/components/AnimatedCheckbox.tsx
@@ -18,13 +18,54 @@ const checkMarkPath =
 const outlineBoxPath =
   'M24 0.5H40C48.5809 0.5 54.4147 2.18067 58.117 5.88299C61.8193 9.58532 63.5 15.4191 63.5 24V40C63.5 48.5809 61.8193 54.4147 58.117 58.117C54.4147 61.8193 48.5809 63.5 40 63.5H24C15.4191 63.5 9.58532 61.8193 5.88299 58.117C2.18067 54.4147 0.5 48.5809 0.5 40V24C0.5 15.4191 2.18067 9.58532 5.88299 5.88299C9.58532 2.18067 15.4191 0.5 24 0.5Z'
 
+const AnimatedPath = Animated.createAnimatedComponent(Path)
+
+interface Props {
+  checked?:boolean
+}
+
+const AnimatedCheckbox = (props:Props) =>{
+
+  const {checked} = props
+    const checkMarkColor = '#000000'
+    const highLightColor =  '#ff0000'
+    const boxOutlineColor = '#000000'
 
 
-const AnimatedCheckbox = () =>{
+    const progress = useSharedValue(0)
+
+    useEffect(() => {
+      progress.value = withTiming(checked ? 1 : 0, {
+        duration: checked ? 300 : 100,
+        easing: Easing.linear
+      })
+    }, [checked])
+
+    const animatedBoxProps = useAnimatedProps(
+      () => ({
+        stroke: interpolateColor(
+          Easing.bezierFn(0.16, 1, 0.3, 1)(progress.value),
+          [0, 1],
+          [boxOutlineColor, highLightColor],
+          'RGB'
+        ),
+        fill: interpolateColor(
+          Easing.bezierFn(0.16, 1, 0.3, 1)(progress.value),
+          [0, 1],
+          ['#00000000', highLightColor],
+          'RGB'
+        )
+      }),
+      [highLightColor, boxOutlineColor]
+    )
+
+    console.log(progress.value)
+    
     return(
         <Svg viewBox={[-MARGIN,-MARGIN,vWidth+MARGIN,vHeight+MARGIN].join(' ')}>
+            
+            <AnimatedPath d={outlineBoxPath} strokeWidth={7} strokeLinejoin="round" strokeLinecap="round" animatedProps={animatedBoxProps}/>
             <Path d={checkMarkPath} stroke='black'/>
-            <Path d={outlineBoxPath} stroke='black'/>
         </Svg>
     )
 }
